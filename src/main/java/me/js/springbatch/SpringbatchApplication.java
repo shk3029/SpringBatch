1e9d0d58459ed8baddc380d06f3cd3e73b043641
JS <shk3029>
2019-09-29 13:43:10 +0900

    Initial commit

---
+++ src/main/java/me/js/springbatch/SpringbatchApplication.java
@@ -0,0 +1,13 @@
+package me.js.springbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableBatchProcessing // SpringBatch 기능 활성화
+@SpringBootApplication
+public class SpringbatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchApplication.class, args);
+    }
+}
